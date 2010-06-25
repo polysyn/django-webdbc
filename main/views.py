224fbf8de1dea79818b84be25a1be4ba0f8f86c9
Jerome Leclanche <adys@azura>
2010-06-25 09:39:41 +0300

    Implement paging

--- main/views.py
+++ main/views.py
@@ -7,6 +7,7 @@ from pywow.wdbc.environment import Environment
 
 
 ENVIRONMENTS = {}
+ITEMS_PER_PAGE = 100
 
 def get_environment(build):
 	build = int(build)
@@ -24,7 +25,31 @@ def tableview(request, build, tablename):
 	if tablename not in env:
 		raise Http404
 	table = env[tablename]
-	return render_to_response("tableview.html", {"build": build, "table": table, "tablename": os.path.basename(table.file.name)})
+	
+	page = request.GET.get("p", 1)
+	try:
+		page = int(page)
+	except ValueError:
+		page = 1
+	
+	data = table[:]
+	start = ITEMS_PER_PAGE * (page-1)
+	stop = start + ITEMS_PER_PAGE
+	count = len(table)
+	if count > ITEMS_PER_PAGE * 1.5:
+		data = table[start:stop]
+	
+	return render_to_response("tableview.html", {
+		"build": build,
+		"table": table,
+		"tablename": os.path.basename(table.file.name),
+		"page": page,
+		"page_max": count / ITEMS_PER_PAGE + 1,
+		"data": data,
+		"min": start + 1,
+		"max": min(count, stop),
+	})
+
 
 def buildview(request, build):
 	return render_to_response("buildview.html", {"build": build, "environment": get_environment(build)})
