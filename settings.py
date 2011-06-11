45c87433891005366725a0b236c5ae7342f6611c
Jerome Leclanche <adys@azura>
2011-06-11 20:34:16 +0100

    Update settings

--- settings.py
+++ settings.py
@@ -7,10 +7,15 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-	# ('Your Name', 'your_email@example.com'),
+	("Jerome Leclanche", "adys.wh@gmail.com"),
 )
 
 MANAGERS = ADMINS
+
+DATABASES = {
+	"default": {
+	}
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -19,11 +24,11 @@ MANAGERS = ADMINS
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = "America/Chicago"
+TIME_ZONE = "Europe/London"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
 SITE_ID = 1
 
@@ -35,20 +40,10 @@ USE_I18N = False
 # calendars according to the current locale
 USE_L10N = False
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static")
-
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = "/media/"
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ""
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -57,21 +52,22 @@ STATIC_URL = "/static/"
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = "/static/admin/"
 
 # Additional locations of static files
 STATICFILES_DIRS = (
 	# Put strings here, like "/home/html/static" or "C:/www/django/static".
 	# Always use forward slashes, even on Windows.
 	# Don't forget to use absolute paths, not relative paths.
+	os.path.join(os.path.dirname(os.path.abspath(__file__)), "static"),
 )
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-	'django.contrib.staticfiles.finders.FileSystemFinder',
-	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#	'django.contrib.staticfiles.finders.DefaultStorageFinder',
+	"django.contrib.staticfiles.finders.FileSystemFinder",
+	"django.contrib.staticfiles.finders.AppDirectoriesFinder",
+#	"django.contrib.staticfiles.finders.DefaultStorageFinder",
 )
 
 # Make this unique, and don't share it with anybody.
@@ -85,10 +81,16 @@ TEMPLATE_DIRS = (
 TEMPLATE_LOADERS = (
 	"django.template.loaders.filesystem.Loader",
 	"django.template.loaders.app_directories.Loader",
+#	 "django.template.loaders.eggs.Loader",
 )
 
 MIDDLEWARE_CLASSES = (
 	"django.middleware.common.CommonMiddleware",
+	#"django.contrib.sessions.middleware.SessionMiddleware",
+	#"django.middleware.csrf.CsrfViewMiddleware",
+	#"django.contrib.auth.middleware.AuthenticationMiddleware",
+	#"django.contrib.messages.middleware.MessageMiddleware",
+	#"django.contrib.flatpages.middleware.FlatpageFallbackMiddleware",
 )
 
 ROOT_URLCONF = "webdbc.urls"
@@ -96,3 +98,35 @@ ROOT_URLCONF = "webdbc.urls"
 INSTALLED_APPS = (
 	"webdbc.main",
 )
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+	"version": 1,
+	"disable_existing_loggers": False,
+	"handlers": {
+		"mail_admins": {
+			"level": "ERROR",
+			"class": "django.utils.log.AdminEmailHandler"
+		}
+	},
+	"loggers": {
+		"django.request": {
+			"handlers": ["mail_admins"],
+			"level": "ERROR",
+			"propagate": True,
+		},
+	}
+}
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+# Unversioned local overrides
+try:
+	from settings_override import *
+except ImportError:
+	pass
