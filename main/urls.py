09110a1806d9e16c9d188db8cedfac7805cf8795
Jerome Leclanche <adys@azura>
2011-06-11 20:34:28 +0100

    Fix tablename regex

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns("webdbc.main.views",
-	(r"^(?P<build>\d+)/(?P<tablename>\w+)/$", "tableview"),
+	(r"^(?P<build>\d+)/(?P<tablename>[^/]+)/$", "tableview"),
 	(r"^(?P<build>\d+)/$", "buildview"),
 )
